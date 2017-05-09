ece11405ec0519dc3409bc5ddbfe63d1c2017e71
Thomas Vuylsteke <thovuy@microsoft.com>
2017-05-09 17:15:43 +0200

    Update importAllConfigurations.ps1
    added $PSScriptRoot to the path for both the configfile and dataconfig file.

--- Automation/Scripts/importAllConfigurations.ps1
+++ Automation/Scripts/importAllConfigurations.ps1
@@ -11,7 +11,7 @@ $cfgPath = "$PSScriptRoot/../Configurations"
 
 Get-ChildItem -Path $cfgPath | Where-Object { $_.Name -notlike "*ConfigData*" } | Where-Object { $_.Name -like "*.ps1" } | ForEach-Object {
     Write-Information -MessageData "Processing $_.Name"
-    $dscConfigFile = "../Configurations/$($_.Name)"
-    $dscConfigDataFile = "../Configurations/$($_.BaseName)-ConfigData.ps1"
+    $dscConfigFile = "$PSScriptRoot/../Configurations/$($_.Name)"
+    $dscConfigDataFile = "$PSScriptRoot/../Configurations/$($_.BaseName)-ConfigData.ps1"
     . $PSScriptRoot"/importConfiguration.ps1" -dscConfigFile $dscConfigFile -dscDataConfigFile $dscConfigDataFile -dscAutomationAccount $dscAutomationAccount -dscResourceGroup $dscResourceGroup -Force $Force
 }
